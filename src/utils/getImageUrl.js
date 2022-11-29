3e70c22f816d6c16f73f550145d19c8c6911a3c9
Anton Voitenko <anton.voytenko@xtmotion.co.uk>
2022-11-29 16:05:34 +0100

    index param to indicate that user is in list and thumbnail only is required

--- src/utils/getImageUrl.js
+++ src/utils/getImageUrl.js
@@ -2,4 +2,4 @@ import {useApiUrl} from '../composables/useApiUrl.js';
 import buildURLQuery from './buildURLQuery.js';
 const {apiUrl} = useApiUrl();
 
-export const getImageUrl = (adapter, path) => apiUrl.value + '?' + buildURLQuery({q: 'preview', adapter, path});
+export const getImageUrl = (adapter, path, index = '') => apiUrl.value + '?' + buildURLQuery({q: 'preview', adapter, path, index});
